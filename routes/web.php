573aa1141089c874ede7e8345f8fe9ca26c60965
ayatramy <ayatramy739@gmail.com>
2025-02-20 22:15:36 +0200

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function(){
+    $name='Ayat Ramy';
+    $departments = [
+        '1'=>'Tichnical',
+        '2'=>'Financial',
+        '3'=>'Sales',
+    ];
+    //return view('about', ['name' => $name]);
+    //return view('about')->with('name',$name);
+    return view('about', compact('name', 'departments'));
+});
+
+Route::post('/about', function(){
+    $name = $_POST['name'];
+    $departments = [
+        '1'=>'Tichnical',
+        '2'=>'Financial',
+        '3'=>'Sales',
+    ];
+    return view('about', compact('name'));
+});
